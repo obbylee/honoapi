0b3c9f8daed7f7ac84eb11616d77150fa21244dd
leeobby <leeobbyw@gmail.com>
2025-07-16 16:40:24 +0700

    Add hono openapi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
-import { Hono } from "hono";
+import { Scalar } from "@scalar/hono-api-reference";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
-const app = new Hono().basePath("/api");
+const app = new OpenAPIHono().basePath("/api");
+
+app.use(logger());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get(
+  "/docs",
+  Scalar({
+    pageTitle: "Terra Discover API Documentation",
+    theme: "default",
+    url: "/api/openapi.json",
+  })
+);
+
+app.doc("/openapi.json", {
+  openapi: "3.1.0",
+  info: {
+    version: "0.0.1",
+    title: "Terra Discover API",
+    description: "API for Terra Discover API",
+  },
+  servers: [
+    {
+      url: "https://honoapi-down.vercel.app",
+      description: "API Server",
+    },
+  ],
+});
 
 app.get("/", (c) => {
   return c.json("Hello Hono!");
