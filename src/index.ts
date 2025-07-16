846c06df3fd70b8e3129a0f3ce4aa8c07879cc6c
leeobby <leeobbyw@gmail.com>
2025-07-16 16:42:50 +0700

    Remove hono openapi

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,12 @@
-import { Scalar } from "@scalar/hono-api-reference";
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { logger } from "hono/logger";
-import { cors } from "hono/cors";
+import { Hono } from "hono";
 
-const app = new OpenAPIHono().basePath("/api");
-
-app.use(logger());
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.get(
-  "/docs",
-  Scalar({
-    pageTitle: "Terra Discover API Documentation",
-    theme: "default",
-    url: "/api/openapi.json",
-  })
-);
-
-app.doc("/openapi.json", {
-  openapi: "3.1.0",
-  info: {
-    version: "0.0.1",
-    title: "Terra Discover API",
-    description: "API for Terra Discover API",
-  },
-  servers: [
-    {
-      url: "https://honoapi-down.vercel.app",
-      description: "API Server",
-    },
-  ],
-});
+const app = new Hono().basePath("/api");
 
 app.get("/", (c) => {
-  return c.json("Hello Hono!");
+  return c.text("Hello Hono!");
 });
 
-app.get("/trpc", (c) => {
+app.get("/greet", (c) => {
   return c.json({ message: "connected!" });
 });
 
